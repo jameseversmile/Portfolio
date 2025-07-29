4c0c4ef12d35ba21044463c3adec0ac622aae76a
jameseversmile <jameseversmile@gmail.com>
2025-07-29 15:18:42 +0100

    my portfolio

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -7,12 +7,13 @@ import { FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="flex gap-10 justify-center my-10">
-        <FaGithub href="https://x.com/farmacrypt" />
-        <CiLinkedin href="https://x.com/farmacrypt" />
-        <FaXTwitter href="https://x.com/farmacrypt" />
-        <FaFacebookSquare href="https://x.com/farmacrypt" />
-        
+    <div className="pb-2.5">
+        <div className="flex gap-10 justify-center my-10">
+           <FaGithub href="https://x.com/farmacrypt" />
+           <CiLinkedin href="https://x.com/farmacrypt" />
+           <FaXTwitter href="https://x.com/farmacrypt" />
+           <FaFacebookSquare href="https://x.com/farmacrypt" />
+        </div>
     </div>
   )
 }
